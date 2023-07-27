d134786f774573f6b68fced8fb7093bba5b35a5d
Solpio <ssusolpio@gmail.com>
2023-07-27 12:58:13 +0400

    add create user method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/user/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userModel.create(dto);
+    return user;
+  }
+}
