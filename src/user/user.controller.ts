a8c7a6dbea3c6a739c8d483c9fe03d379813a22b
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-05 01:24:37 -0300

    deploy

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,5 +3,7 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+    console.log("For test");
+  }
 }
