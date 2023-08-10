15c3693226426f20c2feaefec5c8b7b1c04e27df
Solpio <ssusolpio@gmail.com>
2023-08-10 21:34:01 +0400

    add dto

---
+++ src/business/dto/create-business.dto.ts
@@ -0,0 +1,21 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateBusinessDto {
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly contacts: string;
+
+  @IsString()
+  @IsOptional()
+  readonly description: string;
+
+  @IsString()
+  @IsOptional()
+  readonly address: string;
+
+  @IsString()
+  @IsOptional()
+  readonly preview: string;
+}
