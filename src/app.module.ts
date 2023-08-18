74eee39d7fef4346631f0332a7d2ca98839ba3e6
Solpio <ssusolpio@gmail.com>
2023-08-18 07:16:08 +0400

    add s3

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,23 @@ import { QrcodeModule } from './qrcode/qrcode.module';
 import { PaymentModule } from './payment/payment.module';
 import { CryptomusModule } from './cryptomus/cryptomus.module';
 import { BusinessModule } from 'src/business/business.module';
+import { FileModule } from './file/file.module';
+import { S3Module } from 'nestjs-s3';
 
 @Module({
   imports: [
+    S3Module.forRootAsync({
+      useFactory: () => ({
+        config: {
+          accessKeyId: process.env.S3_ACCESS_KEY,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+          region: process.env.S3_REGION,
+          endpoint: process.env.S3_ENDPOINT,
+          s3ForcePathStyle: true,
+          // signatureVersion: 'v4',
+        },
+      }),
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     BotModule,
     UserModule,
@@ -22,6 +36,7 @@ import { BusinessModule } from 'src/business/business.module';
     PaymentModule,
     CryptomusModule,
     BusinessModule,
+    FileModule,
   ],
   controllers: [],
   providers: [],
