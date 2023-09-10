ad6ca63e2132357c619514e53a69b0a626a41b4b
Petr Molchanov <p.molchanov@profsoft.pro>
2023-09-10 17:52:00 -0300

    ddos defer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -11,6 +16,18 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  app.use(helmet());
+  app.use(xss());
+  app.use(hpp());
+  app.use(mongoSanitize());
+
+  const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+  });
+
+  app.use(limiter);
 
   await app.listen(3000);
 }
