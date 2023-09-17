bd2cddab608c3cf1f1c7ed2cd9ea6e466d71fa07
Solpio <ssusolpio@gmail.com>
2023-09-17 06:03:25 +0400

    add sub method

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 
 @Controller('subscription')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
+  @Get(':userId')
+  findSubscription(
+    @Query('isActive') isActive: boolean,
+    @Param('userId') userId,
+  ) {
+    return this.subscriptionService.findSubscription(userId, isActive);
+  }
 }
