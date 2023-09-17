0aaefffbe4a1d3fcb16c0ba2641c01e7c9acacbe
Solpio <ssusolpio@gmail.com>
2023-09-18 00:36:28 +0400

    refactor

--- src/cryptomus/cryptomus.module.ts
+++ src/cryptomus/cryptomus.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { CryptomusService } from './cryptomus.service';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cryptomus, CryptomusSchema } from 'src/cryptomus/cryptomus.schema';
 
 @Module({
   imports: [
+    MongooseModule.forFeature([
+      { name: Cryptomus.name, schema: CryptomusSchema },
+    ]),
     HttpModule.register({
       timeout: 5000,
       maxRedirects: 5,
