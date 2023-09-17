7cdd9bd1ca6d8b5501d81f16bed2f5948c15925b
Solpio <ssusolpio@gmail.com>
2023-09-17 06:37:51 +0400

    refactor

--- src/tariff/tariff.controller.ts
+++ src/tariff/tariff.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TariffService } from 'src/tariff/tariff.service';
+import { CreateTariffDto } from 'src/tariff/dto/create-tariff.interface';
 
 @Controller('tariff')
 export class TariffController {
   constructor(private readonly tariffService: TariffService) {}
-  // @Post()
-  // async createTariff(@Body() body: CreateTariffDto) {
-  //   return this.tariffService.createTariff(body);
-  // }
+  @Post()
+  async createTariff(@Body() body: CreateTariffDto) {
+    return this.tariffService.createTariff(body);
+  }
 }
